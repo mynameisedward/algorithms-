0f51bab695b40e9efde09532ba3ce6a53e0b8ae7
Edward <snegovikzimniy@gmail.com>
2025-09-29 19:00:26 +0300

    lection2: task a

---
+++ Тренировки 1.0/Лекция 2. Линейный поиск/Задачи ДЗ/B.js
@@ -0,0 +1,20 @@
+function algo(arr) {
+
+    let output = 'CONSTANT'
+    let lastNumber = arr[0]
+
+
+    for(let i in arr) {
+        if(i == 0) {
+            continue
+        } else if(i == arr.length - 1) {
+            break
+        }
+        // if(arr[i] >= )
+
+    }
+
+    console.log(output)
+}
+
+let test = [1, 1, 1, 1, 1]
