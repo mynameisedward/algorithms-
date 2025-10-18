eef957be4972b11cebd4891c5c81229f29802c24
Edward <snegovikzimniy@gmail.com>
2025-10-18 16:20:37 +0300

    leetcode problem: backspace string compare(easy)

--- leetcode problems/BackSpace String Compare.js
+++ leetcode problems/BackSpace String Compare.js
@@ -6,53 +6,38 @@
 // Output: true
 // Explanation: Both s and t become "ac".
 
-function backspaceCompare(s1, s2) {
-    let p1 = s1.length - 1
-    let p2 = s2.length - 1
-    let p1BP = 0
-    let p2BP = 0
-
-    while((p1 >= 0) && (p2 >= 0)) {
-        if((s1[p1] == '#')) {
-            p1BP += 1
-            p1 -= 1
-        } else if((s1[p1] != '#') && (p1BP > 0)) {
-            if(s1[p1 - 1] == '#') {
-                p1 -= 1
-                p1BP -= 1
-            } else {
-                p1 -= p1BP
-                p1BP = 0
-            }
-        }
-
-        if((s2[p2] == '#')) {
-            p2BP += 1
-            p2 -= 1
-        } else if((s2[p2] != '#') && (p2BP > 0)) {
-            if(s1[p1 - 1] == '#') {
-                p2 -= 1
-                p2BP -= 1
-            } else {
-                p2 -= p2BP
-                p2BP = 0
-            }
-        }
-
-        if(((s1[p1] != '#') && (p1BP == 0)) && ((s2[p2] != '#') && (p2BP == 0)) && (s1[p1] != s2[p2])) {
-            return false
+function getCurrentIndex(s, i) {
+    let to_skip = 0
+    while(i >= 0) {
+        if(s[i] == '#') {
+            to_skip += 1
+        } else if((to_skip > 0) && (s[i] != '#')) {
+            to_skip -= 1
         } else {
-            p1 -= 1
-            p2 -= 1
+            break
         }
-    }
 
-    return true
+        i -= 1
+    }
+    return i
 }
 
-console.log(backspaceCompare("asd###asss#s", "as##g#asss"))
+function backspaceCompare(s1, s2) {
+    let i = s1.length - 1
+    let j = s2.length - 1
 
-// *      
-// asd###asss#s
+    while((i >= 0) || (j >= 0)) {
 
-// das##g##asss
+    }
+
+    
+}
+
+console.log(backspaceCompare("bxj##tw", "bxo#j##tw"))
+
+//  *
+// "bxj##tw"
+//  btw
+
+//      *
+// "bxo#j##tw"
