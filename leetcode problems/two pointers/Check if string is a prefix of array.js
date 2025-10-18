af0fab92c806ef36abcdecb08e4ad4879c3352ff
Edward <snegovikzimniy@gmail.com>
2025-10-18 17:39:54 +0300

    leetcode problem: check if string is a prefix of array(easy)

---
+++ leetcode problems/two pointers/Check if string is a prefix of array.js
@@ -0,0 +1,30 @@
+// Example:
+
+// Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+// Output: true
+// Explanation:
+// s can be made by concatenating "i", "love", and "leetcode" together
+
+
+function isPrefixString(s, words) {
+    let sPointer = 0;
+
+    for(let i in words) {
+        for(let j = 0; j <= words[i].length - 1; j++) {
+            if((sPointer == s.length) && (j == 0)) {
+                break
+            }
+            if(words[i][j] != s[sPointer]) {
+                return false
+            } else if(sPointer <= s.length ) {
+                sPointer++
+            } 
+        }  
+    }
+
+
+    return s.length == sPointer
+}
+
+console.log(isPrefixString('z', ['z']))
+console.log('------')
