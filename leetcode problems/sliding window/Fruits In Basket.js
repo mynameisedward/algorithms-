15dd05e545e0a619af07639d1595c9737fde7301
Edward <snegovikzimniy@gmail.com>
2025-10-20 20:44:50 +0300

    lection 02-sliding window

---
+++ leetcode problems/sliding window/Fruits In Basket.js
@@ -0,0 +1,19 @@
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+
+
+function totalFruit(fruits) {
+    let begin = 0
+    let window_state
+    let result = []
+
+    for(let end = 0; end < fruits.length; end++) {
+    }
+
+    return result.length
+}
+
+
+
---
+++ leetcode problems/sliding window/pattern.md
@@ -0,0 +1,18 @@
+begin = 0
+window_state
+result 
+
+for(let end = 0; end < nums.length; end++) {
+    window_state
+    window_size = end - begin + 1
+    if // window condition 
+        result 
+        window_state
+        begin += 1 // window srhink
+}
+
+return result
+
+
+Самое главное это понять, что такое состояние окна
+И понять, когда мы поджимаем окно
