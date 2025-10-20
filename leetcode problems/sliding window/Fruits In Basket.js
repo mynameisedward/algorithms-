8398e18c22b88afb64d2863ad7eff4bf637f8780
Edward <snegovikzimniy@gmail.com>
2025-10-20 21:54:36 +0300

    leetcode problem: fruits into basket(medium)

--- leetcode problems/sliding window/Fruits In Basket.js
+++ leetcode problems/sliding window/Fruits In Basket.js
@@ -3,17 +3,31 @@
 // Explanation: We can pick from trees [2,3,2,2].
 // If we had started at the first tree, we would only pick from trees [1,2].
 
-
 function totalFruit(fruits) {
     let begin = 0
-    let window_state
-    let result = []
+    let window_state = {}
+    let result = 0
 
     for(let end = 0; end < fruits.length; end++) {
+        if(!(fruits[end] in window_state)) {
+            window_state[fruits[end]] = 1
+        } else {
+            window_state[fruits[end]] += 1
+        }
+
+        while(Object.keys(window_state).length > 2) {
+            window_state[fruits[begin]] -= 1
+            if(window_state[fruits[begin]] == 0) {
+                delete window_state[fruits[begin]]
+            }    
+            begin += 1
+        }
+
+        if(result < (end - begin + 1)) {
+            result = end - begin + 1
+        }
     }
 
-    return result.length
+    return result
 }
-
-
 
