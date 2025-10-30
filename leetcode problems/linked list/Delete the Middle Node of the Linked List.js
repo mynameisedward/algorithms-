832c649fd5a03ac8bca333c05fa56b08f06c7015
Edward <snegovikzimniy@gmail.com>
2025-10-30 17:05:29 +0300

    leetcode problem: Delete the Middle Node of the Linked List(medium)

---
+++ leetcode problems/linked list/Delete the Middle Node of the Linked List.js
@@ -0,0 +1,15 @@
+// Задача: удалить центральную ноду связанного списка
+
+// Input: head = [1,3,4,7,1,2,6]
+// Output: [1,3,4,1,2,6]
+// Explanation:
+// The above figure represents the given linked list. The indices of the nodes are written below.
+// Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+// We return the new list after removing this node. 
+
+// Решение: Решаем также через быстрый и медленный, но даем фору быстрому в 2 шага
+//          Медленный тогда оказывается за один элемент до середины и мы спокойно удаляем центральную ноду
+
+
+
+    
