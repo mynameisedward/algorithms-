628a15b62dbe28b56a1a792fc5e23a298999ae55
Edward <snegovikzimniy@gmail.com>
2025-10-30 21:12:30 +0300

    leetcode problem: Remove Duplicates from Sorted Array(easy)

---
+++ leetcode problems/linked list/Remove Duplicates from Sorted List.js
@@ -0,0 +1,19 @@
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+function deleteDuplicates(head) {
+    if(!(head)) {
+        return null
+    }
+
+    let current = head
+    while(current.next) {
+        if(current.next.val === current.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    }
+    
+    return head
+}
