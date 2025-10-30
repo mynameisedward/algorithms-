9ededb6e2b925cb51abd1780e81c3647682c560a
Edward <snegovikzimniy@gmail.com>
2025-10-30 17:48:13 +0300

    leetcode problem: Reverse Linked List(easy)

---
+++ leetcode problems/linked list/Reverse Linked List.js
@@ -0,0 +1,19 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Решение: переворачиваем поочередно ссылки
+
+function reverseList(head) {
+    let prev = null
+    let current = head
+
+    while(current) {
+        let tmp = current.next
+        current.next = prev
+        prev = current
+        current = tmp
+    }
+    return prev
+}
