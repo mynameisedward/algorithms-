3d9890f0886ac3cb01ac4a0b6c13ec6025d009ab
Edward <snegovikzimniy@gmail.com>
2025-11-07 21:30:40 +0300

    leetcode problem: add two numbers(medium)

---
+++ leetcode problems/linked list/Add Two Numbers.js
@@ -0,0 +1,90 @@
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let head = new ListNode()
+    let current = head
+
+    let p1 = l1
+    let p2 = l2
+
+    let tmp = 0
+    while (p1 && p2) {
+        let newNode = new ListNode()
+        let sum = p1.val + p2.val + tmp
+        if (sum > 9) {
+            newNode.val = Number(String(sum)[1])
+            tmp = 1
+        } else {
+            newNode.val = sum
+            tmp = 0
+        }
+        current.next = newNode
+        current = newNode
+        p1 = p1.next
+        p2 = p2.next
+    }
+
+    // добавить остаток
+    while (p1) {
+        if (tmp === 0) {
+            current.next = p1
+            break
+        } else {
+            let newNode = new ListNode()
+            let sum = p1.val + tmp
+            if (sum > 9) {
+                newNode.val = Number(String(sum)[1])
+                tmp = 1
+            } else {
+                newNode.val = sum
+                tmp = 0
+            }
+            current.next = newNode
+            current = newNode
+            p1 = p1.next
+        }
+    }
+
+    while (p2) {
+        if (tmp === 0) {
+            current.next = p2
+            break
+        } else {
+            let newNode = new ListNode()
+            let sum = p2.val + tmp
+            if (sum > 9) {
+                newNode.val = Number(String(sum)[1])
+                tmp = 1
+            } else {
+                newNode.val = sum
+                tmp = 0
+            }
+            current.next = newNode
+            current = newNode
+            p2 = p2.next
+        }
+    }
+
+    if (tmp) {
+        let newNode = new ListNode(1, null)
+        current.next = newNode
+        current = newNode
+    }
+
+
+    return head.next
+};
---
+++ leetcode problems/linked list/Linked List Cycle.js
@@ -0,0 +1,17 @@
+
+
+function hasCycle(head) {
+    let slow = head
+    let fast = head
+
+    while(fast && fast.next) {
+        slow = slow.next
+        fast = fast.next
+
+        if(slow == fast) {
+            return true
+        }
+    }
+
+    return false
+}
