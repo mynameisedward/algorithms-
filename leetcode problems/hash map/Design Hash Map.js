463249319ad28ae187d19451aa7dac8e9a616774
Edward <snegovikzimniy@gmail.com>
2025-11-10 18:12:13 +0300

    leetcode problem: Design Hash Map(easy)

---
+++ leetcode problems/hash map/Design Hash Map.js
@@ -0,0 +1,47 @@
+var MyHashMap = function() {
+    this.size = 743
+    this.buckets = Array(this.size).fill(null)
+    for(let i = 0; i < this.size; i++) {
+        this.buckets[i] = []
+    }
+}
+
+MyHashMap.prototype.hash = function(key) {
+    return key % this.size
+}
+
+MyHashMap.prototype.get = function(key) {
+    const index = hash(key)
+    const bucket = this.buckets[index]
+
+    for(let pair of bucket) {
+        if(pair[0] == key) {
+            return pair[1]
+        }
+    }
+    return -1
+}
+
+MyHashMap.prototype.put = function(key, value) {
+    let index = hash(key)
+    let bucket = this.buckets[index]
+
+    for(let pair of bucket) {
+        if(pair[0] == key) {
+            pair[1] = value
+            return
+        }
+    }
+    bucket.push([key, value])
+}
+
+MyHashMap.prototype.remove = function(key) {
+    let index = hash(key)
+    let bucket = this.buckets[index]
+
+    for(let i = 0; i < bucket.length; i++) {
+        if(bucket[i][0] == key) {
+            bucket.splice(i, 1)
+        }
+    }
+}
