4b85148052e359d7af726092585ca65d27e519e1
Edward <snegovikzimniy@gmail.com>
2025-11-14 13:41:27 +0300

    leetcode problem: Simplify Path(medium)

---
+++ leetcode problems/Stack, Queue/Simplify Path.js
@@ -0,0 +1,23 @@
+let string = "/home/user//Documents/../Pictures"
+
+let withoutSlash = string.split('/')
+
+console.log(withoutSlash)
+
+let stack = []
+
+for(let elem of withoutSlash) {
+    if((elem == '') || (elem == '/')) {
+        continue
+    } else if(elem == '..') {
+        if(stack.length > 0) {
+            stack.pop()
+        }
+    } else {
+        stack.push(elem)
+    }
+}
+
+const result = '/' + stack.join('/')
+
+// console.log(result)
