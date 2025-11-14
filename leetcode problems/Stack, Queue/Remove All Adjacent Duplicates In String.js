e2f51d269cb19e69280b3bd9d6677a40716fbd8f
Edward <snegovikzimniy@gmail.com>
2025-11-14 12:04:59 +0300

    leetcode problem: Remove All Adjacent Duplicates In String(easy)

---
+++ leetcode problems/Stack, Queue/Remove All Adjacent Duplicates In String.js
@@ -0,0 +1,13 @@
+function removeDuplicates(s) {
+    let stack = []
+
+    for(let char of s) {
+        if(stack[stack.length - 1] == char) {
+            stack.pop()
+        } else {
+            stack.push(char)
+        }
+    }
+
+    return stack.join("")
+}
