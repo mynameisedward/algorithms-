f2b4cb99fc171ccd9fe0388e91c174dde39affb4
Edward <snegovikzimniy@gmail.com>
2025-11-14 12:24:30 +0300

    leetcode problem: Removing Stars From a String(medium)

---
+++ leetcode problems/Stack, Queue/Removing Stars From a String.js
@@ -0,0 +1,15 @@
+function removeStars(s) {
+    let stack = []
+
+    for(let char of s) {
+        if(char == '*') {
+            stack.pop()
+        } else {
+            stack.push(char)
+        }
+    }
+
+    return stack.join("")
+}
+
+
