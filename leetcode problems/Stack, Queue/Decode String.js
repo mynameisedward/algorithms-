30c519f73560816e81cb786b15701eb7bac4cd5b
Edward <snegovikzimniy@gmail.com>
2025-11-17 20:08:23 +0300

    leetcode problem: Decode String(medium)

---
+++ leetcode problems/Stack, Queue/Decode String.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let stack = []
+    for(let char of s) {
+        if(char !== ']') {
+            stack.push(char)
+            continue
+        }
+        let last = stack.pop()
+        let str = ''
+        while(last !== '[') {
+            str = last + str
+            last = stack.pop()
+        }
+        let numb = ''
+        last = stack.pop()
+        while(!Number.isNaN(Number(last))) {
+            numb = last + numb
+            last = stack.pop()
+        }
+        stack.push(last)
+        stack.push(str.repeat(Number(numb)))
+    }
+    return stack.join("")
+};
