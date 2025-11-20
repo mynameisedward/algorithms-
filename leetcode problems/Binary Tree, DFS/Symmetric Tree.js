dc0de3484f71c8b790565e818ac0450142f49e2a
Edward <snegovikzimniy@gmail.com>
2025-11-20 21:22:24 +0300

    leetcode problem: Symmetric Tree(easy)

---
+++ leetcode problems/Binary Tree, DFS/Symmetric Tree.js
@@ -0,0 +1,25 @@
+function isSymmetric(root) {
+    const stack = [root.left, root.right]
+
+    while(stack.length > 0) {
+        const right = stack.pop()
+        const left = stack.pop()
+        if(right && left) {
+            if(right.val === left.val) {
+                stack.push(right.right)
+                stack.push(left.left)
+                stack.push(right.left)
+                stack.push(left.right)
+            } else {
+                return false
+            }
+        } else if(!right && !left) {
+            continue
+        } else {
+            return false
+        }
+    }
+
+
+    return true
+}
