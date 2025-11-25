527b58ced96b00fccbc403ca325bdeed292d632b
Edward <snegovikzimniy@gmail.com>
2025-11-25 20:00:25 +0300

    leetcode problem: Insert Into a Binary Search Tree(medium)

---
+++ leetcode problems/Binary Search Tree/Insert into a Binary Search Tree.js
@@ -0,0 +1,31 @@
+function intsertIntoBST(root, val) {
+    if(!root) {
+        return new TreeNode(val)
+    } 
+
+    const stack = [root]
+
+    while(stack.length > 0) {
+        const node = stack.pop()
+
+        if(val < node.val) {
+            if(node.left) {
+                stack.push(node.left)
+            } else {
+                const newNode = new TreeNode(val)
+                node.left = newNode
+                break
+            }
+        } else {
+            if(node.right) {
+                stack.push(node.right)
+            } else {
+                const newNode = new TreeNode(val)
+                node.right = newNode
+                break
+            }
+        }
+    }
+
+    return root
+}
