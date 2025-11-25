8467da6c23c438a93f97144cd73171393faea576
Edward <snegovikzimniy@gmail.com>
2025-11-25 19:35:06 +0300

    leetcode problems: Search in Binary Search Tree(easy)

---
+++ leetcode problems/Binary Search Tree/Search in a Binary Search Tree.js
@@ -0,0 +1,38 @@
+// Using stack
+function searchBST(root, val) {
+    const stack = [root]
+
+    while(stack.length > 0) {
+        const node = stack.pop()
+        if(!node) {
+            continue
+        }
+        if(node.val === val) {
+            return node
+        } else if(val < node.val) {
+            stack.push(node.left)
+        } else if(val > node.val) {
+            stack.push(node.right)
+        }
+    }
+
+    return null
+}
+
+
+
+// Using recursion
+function searchBST(root, val) {
+    if(!root) {
+        return null
+    }
+    if(root.val === val) {
+        return root
+    }
+
+    if(val < root.val) {
+        return searchBST(root.left)
+    } else {
+        return searchBST(root.right)
+    }
+}
