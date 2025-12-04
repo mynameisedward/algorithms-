c40936f9f8d2f3522f78eac8c6cea4a992468f59
Edward <snegovikzimniy@gmail.com>
2025-12-04 19:06:25 +0300

    leetcode problem: Merge Sorted Array(easy)

--- leetcode problems/two pointers/Merge Sorted Array.js
+++ leetcode problems/two pointers/Merge Sorted Array.js
@@ -1,36 +1,19 @@
+// Основная идея: идем с конца первого массива и заполняем числа, сравнивая
+
+
 function merge(nums1, m, nums2, n) {
-    let result = []
-    let p1 = 0
-    let p2 = 0
+    let firstPointer = m - 1
+    let secondPointer = n - 1
+    let rightPointer = m + n - 1
 
-    while((p1 < m) || (p2 < n)) {
-        if(p1 == m) {
-            result.push(nums2[p2])
-            p2 += 1
-            continue
-        } else if(p2 == n) {
-            result.push(nums1[p1])
-            p1 += 1
-            continue
-        }
-        if(nums1[p1] < nums2[p2]) {
-            result.push(nums1[p1])
-            p1 += 1
+    while(secondPointer >= 0) {
+        if(firstPointer >= 0 && nums1[firstPointer] > nums2[secondPointer]) {
+            nums1[rightPointer] = nums1[firstPointer]
+            firstPointer -= 1
         } else {
-            result.push(nums2[p2])
-            p2 += 1
+            nums1[rightPointer] = nums2[secondPointer]
+            secondPointer -= 1
         }
+        right -= 1
     }
-    
-    for(let i in result) {
-        nums1[i] = result[i]
-    }
-
-    console.log(result)
-
-    return
 }
-
-
-
-merge([1,2,3,0,0,0,0], 3, [2,5,6,7], 4)
