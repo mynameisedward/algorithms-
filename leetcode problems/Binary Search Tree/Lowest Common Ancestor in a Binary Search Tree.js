d40eea821ff2c09f62b813c257837521ddbfbd43
Edward <snegovikzimniy@gmail.com>
2025-12-09 18:04:54 +0300

    leetcode problem(Binary Search Tree): Lowest Common Ancestor in a Binary Search Tree(medium)

---
+++ leetcode problems/Binary Search Tree/Lowest Common Ancestor in a Binary Search Tree.js
@@ -0,0 +1,11 @@
+function lowestCommonAncestor(root, p, q) {
+    while(root) {
+        if(p.val > root.val && q.val > root.val) {
+            root = root.right
+        } else if(p.val < root.val && q.val < root.val) {
+            root = root.left
+        } else {
+            return root
+        }
+    }
+}
