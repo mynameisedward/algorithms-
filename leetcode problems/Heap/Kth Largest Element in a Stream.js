e7915a27796c6fee045f61d5cf4b530e7bdaafae
Edward <snegovikzimniy@gmail.com>
2025-12-16 20:23:38 +0300

    leetcode problems(heap): kth largest element in a stream(easy)

---
+++ leetcode problems/Heap/Kth Largest Element in a Stream.js
@@ -0,0 +1,29 @@
+// Основная идея: Min heap
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.heap = new MinPriorityQueue();
+    this.k = k;
+
+    for(let num of nums) {
+        this.heap.enqueue(num)
+        if(this.heap.size() > k) {
+            this.heap.dequeue()
+        }
+    }
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.heap.enqueue(val)
+    while(this.heap.size() > this.k) {
+        this.heap.dequeue()
+    }
+    return this.heap.front()
+};
